6763d08f40a0839f2e319e47630c345eab090fc9
Matthew Collier <collier.matthew@gmail.com>
2018-09-20 10:57:51 -0400

    Specify mongo collection in test config.

--- test/test.config.js
+++ test/test.config.js
@@ -8,9 +8,9 @@ const path = require('path');
 config.mocha.tests.push(path.join(__dirname, 'mocha'));
 
 // MongoDB
-// config.mongodb.name = 'bedrock_ledger_guard_signature_test';
-// config.mongodb.dropCollections.onInit = true;
-// config.mongodb.dropCollections.collections = [];
+config.mongodb.name = 'bedrock_ledger_validator_signature_test';
+config.mongodb.dropCollections.onInit = true;
+config.mongodb.dropCollections.collections = [];
 
 config['did-client'].methods['veres-one'].hostname =
   'genesis.testnet.veres.one';
